e1001e52f257214aac90626caf659208e144005f
Rila <rilanajjakha@mail.ugm.ac.id>
2024-10-28 23:42:23 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugasbottomnavfragment/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.tugasbottomnavfragment
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.tugasbottomnavfragment.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // setup navigation
+        with(binding) {
+            val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+            val navController = navHostFragment.navController
+
+            bottomNavigationView.setupWithNavController(navController)
+        }
+    }
+}
